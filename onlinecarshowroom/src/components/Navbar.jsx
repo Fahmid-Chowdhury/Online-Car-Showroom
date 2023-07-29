fc3792b7ddae2198f17319e446dd783c488f0787
NotSoLucky <88020849+NOT-S0-LUCKY@users.noreply.github.com>
2023-07-29 17:13:31 +0600

    sssss

--- onlinecarshowroom/src/components/Navbar.jsx
+++ onlinecarshowroom/src/components/Navbar.jsx
@@ -53,16 +53,16 @@ return (
           <span className='nav-item-text'>Services</span>
         </div>
         </Link>
-        <Link to="/contact">
+        <Link to="/about">
         <div className="nav-item-container">
           <div className="nav-item-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-              <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
-              <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
-            </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clipRule="evenodd" />
+          </svg>
+
 
           </div>
-          <span className='nav-item-text'>Contact</span>
+          <span className='nav-item-text'>About</span>
         </div>
         </Link>
         <Link to="/account">
--- onlinecarshowroom/src/pages/HomePage.jsx
+++ onlinecarshowroom/src/pages/HomePage.jsx
@@ -6,7 +6,15 @@ export default function HomePage() {
     return (
         <>
         <div className="flex-1 home-container">
-            <div className="image-container">
+            <div className="text-container">
+            <h1 class="title">Find your dream car
+                <div class="aurora">
+                    <div class="aurora__item"></div>
+                    <div class="aurora__item"></div>
+                    <div class="aurora__item"></div>
+                    <div class="aurora__item"></div>
+                </div>
+            </h1>
             </div>
         </div>
         </>
