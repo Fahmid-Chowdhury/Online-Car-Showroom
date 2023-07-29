0c0075450a64962720dd39566c5899f092feb994
NotSoLucky <88020849+NOT-S0-LUCKY@users.noreply.github.com>
2023-07-29 15:06:58 +0600

    navbar working

--- onlinecarshowroom/src/components/Navbar.jsx
+++ onlinecarshowroom/src/components/Navbar.jsx
@@ -1,35 +1,83 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import profile from "../assets/mystery_avatar.png"
+
 
 export default function Navbar() {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
-  };
 
-  return (
-    <>
-      <div className='w-[300px] bg-gray-900 text-white '>
-        <div>
-          
-        </div>
-        <div className='nav-button-container'>
-          <button className = 'nav-button'>Sign Up</button>
-          <button className = 'nav-button'>Sign In</button>
-        </div>
+return (
+<>
+  <div className='w-[300px] bg-gray-900 text-white '>
+    <div className='text-center logo-container'>
+      <span>ONLINE CAR</span>
+      <span>SHOWROOM</span>
+      {/* <div className='account-placeholder-icon'>
+        <img src={profile} class="img-fluid" alt=""/>
+      </div> */}
+      
 
-        <nav>
-          <div className='flex flex-col justify-between m-10'>
-            
-            <Link to="/" className='nav-link'>Home</Link>
-            <Link to="/car"className='nav-link'>Browse Cars</Link>
-            <Link to="/services"className='nav-link'>Services</Link>
-            <Link to="/contact"className='nav-link'>Contact</Link>
+    </div>
+    <div className='nav-button-container'>
+      <button className='nav-button'>Sign Up</button>
+      <button className='nav-button'>Sign In</button>
+    </div>
+
+    <nav>
+      <div className='flex flex-col justify-between m-10'>
+        <Link to="/">
+        <div className="nav-item-container">
+          <div className="nav-item-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+              <path fillRule="evenodd"
+                d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
+                clipRule="evenodd" />
+            </svg>
           </div>
-        </nav>
+          <span className='nav-item-text'>Home</span>
+        </div>
+        </Link>
+        <Link to="/car">
+        <div className="nav-item-container">
+          <div className="nav-item-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" height="20px" fill="currentColor" viewBox="0 0 512 512">
+              <path
+                d="M135.2 117.4L109.1 192H402.9l-26.1-74.6C372.3 104.6 360.2 96 346.6 96H165.4c-13.6 0-25.7 8.6-30.2 21.4zM39.6 196.8L74.8 96.3C88.3 57.8 124.6 32 165.4 32H346.6c40.8 0 77.1 25.8 90.6 64.3l35.2 100.5c23.2 9.6 39.6 32.5 39.6 59.2V400v48c0 17.7-14.3 32-32 32H448c-17.7 0-32-14.3-32-32V400H96v48c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V400 256c0-26.7 16.4-49.6 39.6-59.2zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" />
+            </svg>
+          </div>
+          <span className='nav-item-text'>Browse cars</span>
+        </div>
+        </Link>
+        <Link to="/services">
+        <div className="nav-item-container">
+          <div className="nav-item-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+              <path
+                d="M4.464 3.162A2 2 0 016.28 2h7.44a2 2 0 011.816 1.162l1.154 2.5c.067.145.115.291.145.438A3.508 3.508 0 0016 6H4c-.288 0-.568.035-.835.1.03-.147.078-.293.145-.438l1.154-2.5z" />
+              <path fillRule="evenodd"
+                d="M2 9.5a2 2 0 012-2h12a2 2 0 110 4H4a2 2 0 01-2-2zm13.24 0a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V9.5zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V9.5a.75.75 0 00-.75-.75h-.01zM2 15a2 2 0 012-2h12a2 2 0 110 4H4a2 2 0 01-2-2zm13.24 0a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
+                clipRule="evenodd" />
+            </svg>
 
+          </div>
+          <span className='nav-item-text'>Services</span>
+        </div>
+        </Link>
+        <Link to="/contact">
+        <div className="nav-item-container">
+          <div className="nav-item-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+              <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
+              <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
+            </svg>
+
+          </div>
+          <span className='nav-item-text'>Contact</span>
+        </div>
+        </Link>
       </div>
-    </>
-  );
+    </nav>
+
+  </div>
+</>
+);
 }
