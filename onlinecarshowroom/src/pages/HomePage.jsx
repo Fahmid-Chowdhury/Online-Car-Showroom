5ed3150f0ebdacb8efb9c988da82ac672a8240ae
NotSoLucky <88020849+not-solucky@users.noreply.github.com>
2023-07-31 13:06:37 +0600

    Home Screen Completed
    -Completed homescreen
    -Added hero animation (3-way)

--- onlinecarshowroom/src/pages/HomePage.jsx
+++ onlinecarshowroom/src/pages/HomePage.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import backgroundCarImage from '../assets/background-car-image.png';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
 
     return (
         <>
         <div className="relative flex flex-col flex-1 home-container">
-            <div className="text-container-title">
-                <h1><span className='magic'>Welcome<br></br> to Online<br></br>Car Showroom</span></h1>
+            <div className=" text-container-title">  
+                <div><h1><span className='magic'>Welcome<br></br> to Online<br></br>Car Showroom</span></h1></div>
+                <div className='subtitle-container'><p>Find your dream car from a wide selection of vehicles.</p></div>
+                <div className='button-container'><Link to='/cars'>
+                    <button className='text-container-button'>Collections</button>
+                </Link></div>
             </div>
-            <div className="text-container-subtitle">
-                <p>Find your dream car from a wide selection of vehicles.</p>
-            </div>
+            
         </div>
         </>
     );
