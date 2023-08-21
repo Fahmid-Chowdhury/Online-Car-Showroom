463eec4034bba56d24823536c7bd5863cc57319c
NotSoLucky <88020849+not-solucky@users.noreply.github.com>
2023-08-21 10:21:56 +0600

    Update enquiry.js

--- organizedServer/routes/enquiry.js
+++ organizedServer/routes/enquiry.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const checkAuthMiddleware = require('../middleware/check-authentication');
 const enquiryController = require('../controllers/enquiryController');
 
-router.post('/enquiry', checkAuthMiddleware.checkAuth, enquiryController.enquiry)
+router.post('/submitenquiry', checkAuthMiddleware.checkAuth, enquiryController.enquiry)
 router.post('/updateEnquiry', checkAuthMiddleware.checkAuth, enquiryController.updateEnquiry)
 router.post('/response', checkAuthMiddleware.checkAuth, enquiryController.response)
 
